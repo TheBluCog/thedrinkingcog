df043400785a46406cda5a316f44fbfe4c080d8f
TheBluCog <george@thedunphys.ca>
2026-05-19 22:39:17 -0400

    Remove unused Geist font dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import { GeistSans } from 'geist/font/sans';
 import { ReactNode } from 'react';
 import './globals.css';
 
 const SITE_NAME = 'The Drinking Cog';
 
 export const metadata = {
-  metadataBase: new URL('https://thedrinkingcog-git-main-theblucogs-projects.vercel.app'),
+  metadataBase: new URL('https://thedrinkingcog.vercel.app'),
   title: {
     default: SITE_NAME,
     template: `%s | ${SITE_NAME}`
@@ -16,16 +15,10 @@ export const metadata = {
   }
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={GeistSans.variable}>
-      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
