01f1c6918b3ff1214d9d0b63cbb19da3179710f5
TheBluCog <george@thedunphys.ca>
2026-05-19 22:26:16 -0400

    Remove stale Shopify and experimental Next config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,4 @@
-export default {
-  experimental: {
-    ppr: true,
-    inlineCss: true,
-    useCache: true
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
